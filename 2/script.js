cddb84e04b727ef358d47e700b96223a2c0af0c6
Adam Orlowski <adamorlowskipoland@icloud.com>
2017-02-06 05:18:43 +0100

    tided up the code

--- 2/script.js
+++ 2/script.js
@@ -1,22 +1,20 @@
 function evenFibNumbers(){
-            var value = 4000000;
-            var sum = 0;
-            var temp = 0;
-            var num2 = 1;
-            var num1 = 1;
+    var value = 4000000;
+    var sum = 0;
+    var temp = 0;
+    var num2 = 1;
+    var num1 = 1;
 
-            while( num2 < value ) {
-                temp = num1 + num2;
-                num1 = num2;
-                num2 = temp;
-                if ( num2 % 2 == 0 ) {
-                    sum += num2;
-                }
-            }
-
-            //document.getElementById("output").value=sum;
-            alert("result ="+sum);
+    while( num2 < value ) {
+        temp = num1 + num2;
+        num1 = num2;
+        num2 = temp;
+        if ( num2 % 2 == 0 ) {
+            sum += num2;
         }
+    }
+    alert("result ="+sum);
+}
 evenFibNumbers();
 
 
